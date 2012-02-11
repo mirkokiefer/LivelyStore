a6d697ddb8b53ba63dbceb71b4ab2bf922f78577
Mirko Kiefer <mail@mirkokiefer.com>
2012-02-11 21:51:03 +0100

    move byte to hex string conversion to LCUtils

--- LivelyStore/LCSHA.c
+++ LivelyStore/LCSHA.c
@@ -15,12 +15,6 @@ LCType typeSHA = {
 
 LCSHARef LCSHACreate(LCBlobRef blobs[], size_t count);
 void computeSHA1(LCBlobRef blobs[], size_t count, LCByte buffer[]);
-char hexDigitToASCIChar(char hexDigit);
-char asciCharToHexDigit(char hexDigit);
-void byteToHexDigits(LCByte input, char* buffer);
-LCByte hexDigitsToByte(char* hexDigits);
-LCStringRef createHexStringFromBlob(LCBlobRef blob);
-LCBlobRef createBlobFromHexString(LCStringRef hexString);
 
 LCSHARef LCSHACreateFromHashableObject(void* object) {
   LCObjectInfoRef info = (LCObjectInfoRef)object;
@@ -89,51 +83,3 @@ void LCSHADealloc(void* object) {
   LCRelease(((LCSHARef)object)->sha);
 }
 
-// utility functions
-char hexDigitToASCIChar(char hexDigit) {
-  if(hexDigit > 9) {
-    hexDigit = hexDigit - 10 + 97; //97 is A
-  } else {
-    hexDigit = hexDigit + 48; //48 is 0
-  }
-  return hexDigit;
-}
-
-char asciCharToHexDigit(char asciChar) {
-  if (asciChar >= 97) { //A = 97
-    return asciChar - 97 + 10; // A -> 10, B -> 11...
-  } else {
-    return asciChar - 48; // if a number char (48=char 0)
-  }
-}
-
-void byteToHexDigits(LCByte input, char* buffer) {
-  buffer[0] = hexDigitToASCIChar(input/16);
-  buffer[1] = hexDigitToASCIChar(input%16);
-}
-
-LCByte hexDigitsToByte(char* hexDigits) {
-  return hexDigits[0]*16 + hexDigits[1];
-}
-
-LCStringRef createHexStringFromBlob(LCBlobRef blob) {
-  LCByte* input = LCBlobDataRef(blob);
-  size_t length = LCBlobLength(blob);
-  char buffer[length*2];
-  for(LCInteger i=0; i<length; i++) {
-    byteToHexDigits(input[i], &buffer[i*2]);
-  }
-  buffer[length*2] = '\0';
-  return LCStringCreate(buffer);
-}
-
-LCBlobRef createBlobFromHexString(LCStringRef hexString) {
-  LCByte buffer[LC_SHA1_Length];
-  for (LCInteger i=0; i<LC_SHA1_Length; i++) {
-    char digit1 = LCStringCharAtIndex(hexString, i*2);
-    char digit2 = LCStringCharAtIndex(hexString, (i*2)+1);
-    char hexDigits[] = {asciCharToHexDigit(digit1), asciCharToHexDigit(digit2)};
-    buffer[i] = hexDigitsToByte(hexDigits);
-  }
-  return LCBlobCreate(buffer, LC_SHA1_Length);
-}
--- LivelyStore/private/LCUtils.h
+++ LivelyStore/private/LCUtils.h
@@ -6,4 +6,11 @@ void LCRetain(void* object);
 void LCRelease(void* object);
 LCInteger LCRetainCount(void* object);
 
+char hexDigitToASCIChar(char hexDigit);
+char asciCharToHexDigit(char hexDigit);
+void byteToHexDigits(LCByte input, char* buffer);
+LCByte hexDigitsToByte(char* hexDigits);
+LCStringRef createHexStringFromBlob(LCBlobRef blob);
+LCBlobRef createBlobFromHexString(LCStringRef hexString);
+
 #endif
