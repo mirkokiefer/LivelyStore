560d64e9ace6a375fc4816a20ac0c88090ff7758
Mirko Kiefer <mail@mirkokiefer.com>
2012-02-19 23:07:13 +0100

    retain objects in LCArray

--- LivelyStore/lib/LCArray.c
+++ LivelyStore/lib/LCArray.c
@@ -13,10 +13,13 @@ LCType typeArray = {
   .dealloc = LCArrayDealloc
 };
 
-LCArrayRef LCArrayCreate(void* objects, size_t length) {
+LCArrayRef LCArrayCreate(void** objects, size_t length) {
   LCArrayRef newArray = malloc(sizeof(struct LCArray) + length * sizeof(void*));
   if (newArray != NULL) {
     newArray->info.type = &typeArray;
+    for(LCInteger i=0; i<length; i++) {
+      LCRetain(objects[i]);
+    }
     memcpy(newArray->objects, objects, length * sizeof(void*));
   }
   return newArray;
--- LivelyStore/lib/LCArray.h
+++ LivelyStore/lib/LCArray.h
@@ -5,7 +5,7 @@
 
 #include "LivelyStore.h"
 
-LCArrayRef LCArrayCreate(void* objects, size_t length);
+LCArrayRef LCArrayCreate(void** objects, size_t length);
 void** LCArrayObjects(LCArrayRef array);
 void* LCArrayObjectAtIndex(LCArrayRef array, LCInteger index);
 size_t LCArrayLength(LCArrayRef array);
