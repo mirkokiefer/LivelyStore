c233dc45a31c0de243644b66c4b9c46c3ab3b2e0
Mirko Kiefer <mail@mirkokiefer.com>
2012-03-05 01:14:26 +0100

    fix bug in LCMutableArrayRemoveIndex

--- LivelyStore/lib/LCMutableArray.c
+++ LivelyStore/lib/LCMutableArray.c
@@ -72,8 +72,9 @@ void LCMutableArrayAddObject(LCMutableArrayRef array, void* object) {
 }
 
 void LCMutableArrayRemoveIndex(LCMutableArrayRef array, LCInteger index) {
+  LCRelease(array->objects[index]);
   size_t objectsToCopy = array->length - (index+1);
-  memmove(&(array->objects[index]), &(array->objects[index+1]), objectsToCopy);
+  memmove(&(array->objects[index]), &(array->objects[index+1]), objectsToCopy*sizeof(void*));
   array->length = array->length-1;
 }
 
