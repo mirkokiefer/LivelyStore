596b360e366676b95c0d54de423fa5af7755d65b
Mirko Kiefer <mail@mirkokiefer.com>
2012-03-06 11:59:20 +0100

    extend LCStore interface

--- LivelyStore/LCStore.h
+++ LivelyStore/LCStore.h
@@ -12,4 +12,11 @@ void LCStoreSetGetDataCallback(LCStoreRef store, LCGetDataCb callback);
 void LCStorePull(LCStoreRef target, LCStoreRef source);
 void LCStorePush(LCStoreRef source, LCStoreRef target);
 void LCStoreCommit(LCStoreRef store, LCStageRef stage);
+void LCStoreHead(LCStoreRef store, char* buffer[]);
+void LCStoreSetHead(LCStoreRef store, char* commit);
+void LCStoreCommitHistory(LCStoreRef store, char* buffer, size_t start, size_t elements);
+size_t LCStoreCommitHistoryLength(LCStoreRef store);
+void LCStoreDataSHA(LCStoreRef store, char* commit, char* path, char* buffer);
+size_t LCStoreDataLength(LCStoreRef store, char* sha);
+void LCStoreData(LCStoreRef store, char* sha, unsigned char buffer[]);
 #endif
