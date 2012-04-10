fbcdd33623d8b8c67744e7087782e1c3a733706b
Mirko Kiefer <mail@mirkokiefer.com>
2012-04-10 17:05:06 +0200

    add LCRemoteRepository

---
+++ LCRemoteRepository.c
@@ -0,0 +1,64 @@
+
+#include "LCRemoteRepository.h"
+
+typedef struct remoteRepoData* remoteRepoDataRef;
+
+static void* remoteRepoInit();
+
+struct remoteRepoData {
+  LCStringRef url;
+  LCStringRef login;
+  LCStringRef password;
+};
+
+void remoteRepoDealloc(LCObjectRef object);
+
+struct LCType typeRemoteRepo = {
+  .name = "LCRemoteRepository",
+  .immutable = true,
+  .initData = remoteRepoInit,
+  .dealloc = remoteRepoDealloc
+};
+
+LCTypeRef LCTypeRemoteRepository = &typeRemoteRepo;
+
+static void* remoteRepoInit() {
+  remoteRepoDataRef data = malloc(sizeof(struct remoteRepoData));
+  if (data) {
+    data->url = NULL;
+    data->login = NULL;
+    data->password = NULL;
+  }
+  return data;
+}
+
+static remoteRepoDataRef remoteRepoData(LCRemoteRepositoryRef repo) {
+  return objectData(repo);
+}
+
+LCRemoteRepositoryRef LCRemoteRepositoryCreate(char *url, char *login, char *password) {
+  remoteRepoDataRef data = remoteRepoInit();
+  data->url = LCStringCreate(url);
+  data->login = LCStringCreate(login);
+  data->password = LCStringCreate(password);
+  return objectCreate(LCTypeRemoteRepository, data);
+}
+
+char* LCRemoteRepositoryUrl(LCRemoteRepositoryRef repo) {
+  return LCStringChars(remoteRepoData(repo)->url);
+}
+
+char* LCRemoteRepositoryLogin(LCRemoteRepositoryRef repo) {
+  return LCStringChars(remoteRepoData(repo)->login);
+}
+
+char* LCRemoteRepositoryPassword(LCRemoteRepositoryRef repo) {
+  return LCStringChars(remoteRepoData(repo)->password);
+}
+
+void remoteRepoDealloc(LCRemoteRepositoryRef repo) {
+  remoteRepoDataRef data = remoteRepoData(repo);
+  objectRelease(data->url);
+  objectRelease(data->login);
+  objectRelease(data->password);
+}
---
+++ LCRemoteRepository.h
@@ -0,0 +1,15 @@
+
+#ifndef LivelyStore_imac_LCRemoteRepository_h
+#define LivelyStore_imac_LCRemoteRepository_h
+
+#include "LivelyStoreInternal.h"
+
+typedef LCObjectRef LCRemoteRepositoryRef;
+extern LCTypeRef LCTypeRemoteRepository;
+
+LCRemoteRepositoryRef LCRemoteRepositoryCreate(char *url, char *login, char *password);
+char* LCRemoteRepositoryUrl(LCRemoteRepositoryRef repo);
+char* LCRemoteRepositoryLogin(LCRemoteRepositoryRef repo);
+char* LCRemoteRepositoryPassword(LCRemoteRepositoryRef repo);
+
+#endif
